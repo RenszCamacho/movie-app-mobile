57c77447f9d3bd4b0532b99cc0591ebd022e6496
Renszo Camacho <varsovia84@gmail.com>
2021-11-23 17:00:57 +0100

    Refactore useMovies hook.

--- src/hooks/useMovies.tsx
+++ src/hooks/useMovies.tsx
@@ -1,31 +1,29 @@
 import {useEffect, useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
 
-import {styles} from '../screens/home/HomeTheme';
 import {Movies, MoviesResponse} from '../types/movie.interface';
 import movieDB from '../api/movieDB';
 
 export default function useMovies(initialState: Movies[]) {
-  const {container, title, button, buttonText} = styles;
   const {navigate} = useNavigation();
   const [movies, setMovies] = useState(initialState);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchData = async () => {
+    const {data} = await movieDB.get<MoviesResponse>('/now_playing');
+
+    setMovies(data.results);
+
+    setIsLoading(false);
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      const {data} = await movieDB.get<MoviesResponse>('/now_playing');
-      console.log(data.results[0].title);
-      setMovies(data.results);
-    };
-
     fetchData();
   }, []);
 
   return {
-    container,
-    title,
-    button,
-    buttonText,
     navigate,
     movies,
+    isLoading,
   };
 }
