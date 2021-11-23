a03588ad016fc3408a5b38da0d7ab6d22e04b136
Renszo Camacho <varsovia84@gmail.com>
2021-11-23 12:05:00 +0100

    Add Navigation on App.

--- App.tsx
+++ App.tsx
@@ -1,28 +1,11 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {StyleSheet, Text, View} from 'react-native';
+import Navigation from './src/navigation/Navigation';
 
 export default function App() {
-  const {container, title} = styles;
-
   return (
     <NavigationContainer>
-      <View style={container}>
-        <Text style={title}>Hello World</Text>
-      </View>
+      <Navigation />
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 50,
-    fontWeight: 'bold',
-  },
-});
