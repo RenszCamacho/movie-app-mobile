4b8311cc258ad61f74d1c59750c6096f618e400c
Renszo Camacho <varsovia84@gmail.com>
2021-11-23 12:16:16 +0100

    Modify MainActivity.java

--- android/app/src/main/java/com/movies/MainActivity.java
+++ android/app/src/main/java/com/movies/MainActivity.java
@@ -1,4 +1,4 @@
-import android.os.Bundle;
+// import android.os.Bundle;
 package com.movies;
 
 import com.facebook.react.ReactActivity;
@@ -9,10 +9,11 @@ public class MainActivity extends ReactActivity {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
-  }
+
+  // @Override
+  // protected void onCreate(Bundle savedInstanceState) {
+  //   super.onCreate(null);
+  // }
 
   @Override
   protected String getMainComponentName() {
