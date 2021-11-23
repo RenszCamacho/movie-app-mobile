8103b0c4f5fca722c1f3c7bb58173330d916bf50
Renszo Camacho <varsovia84@gmail.com>
2021-11-23 12:47:06 +0100

    Add a TouchableOpacity to navigate to Details.

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, TouchableOpacity, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+
 import {styles} from './HomeTheme';
 
 const HomeScreen = () => {
-  const {container, title} = styles;
+  const {container, title, button, buttonText} = styles;
+  const navigation = useNavigation();
 
   return (
     <View style={container}>
       <Text style={title}>Home</Text>
+
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate('DetailsScreen' as never, {} as never)
+        }
+        style={button}>
+        <Text style={buttonText}>Go to Details</Text>
+      </TouchableOpacity>
     </View>
   );
 };
