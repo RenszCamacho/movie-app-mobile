f227d68aaed64c4729e868d13633ea9327a73928
Renszo Camacho <varsovia84@gmail.com>
2021-11-23 12:46:19 +0100

    Add button, and buttonText, in the Styles.

--- src/screens/home/HomeTheme.ts
+++ src/screens/home/HomeTheme.ts
@@ -11,4 +11,15 @@ export const styles = StyleSheet.create({
     fontSize: 50,
     fontWeight: 'bold',
   },
+  button: {
+    marginTop: 20,
+    padding: 20,
+    backgroundColor: '#4286f4',
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
 });
