f54f84d832bb3983b9c30dc92b9fe6464e7331a2
Renszo Camacho <varsovia84@gmail.com>
2021-11-25 12:23:44 +0100

    Add navigation to DetailsScreen.
    - Make a onPress in the TouchableOpacity, navigate to DetailsScreen.

--- src/components/poster/Poster.tsx
+++ src/components/poster/Poster.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Image, View, TouchableOpacity} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import {URL_BASE_IMAGE} from '@env';
 
 import {Movies} from '../../types/movie.interface';
@@ -21,9 +22,13 @@ export default function Poster({
   const {boxShadow, image} = styles;
   const imageWraper = {height, width, marginHorizontal};
   const uri = `${URL_BASE_IMAGE}/${movies.poster_path}`;
+  const {navigate} = useNavigation();
 
   return (
-    <TouchableOpacity activeOpacity={0.9} style={imageWraper}>
+    <TouchableOpacity
+      onPress={() => navigate('DetailsScreen' as never, {} as never)}
+      activeOpacity={0.9}
+      style={imageWraper}>
       <View style={boxShadow}>
         <Image source={{uri}} style={image} />
       </View>
