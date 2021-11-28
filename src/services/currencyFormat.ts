8ae00871a7487f3eca4ad7ea141e2fc5b4b03fd5
Renszo Camacho <varsovia84@gmail.com>
2021-11-28 14:42:01 +0100

    Create currencyFormat function.

---
+++ src/services/currencyFormat.ts
@@ -0,0 +1,8 @@
+const currencyFormat = (value: number) =>
+  new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format(value);
+
+export default currencyFormat;
