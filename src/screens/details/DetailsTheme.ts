ca73ce8f3543c019d89b29aef0f59a6fd674259b
Renszo Camacho <varsovia84@gmail.com>
2021-11-28 12:40:33 +0100

    Refactor DetailsTheme.

--- src/screens/details/DetailsTheme.ts
+++ src/screens/details/DetailsTheme.ts
@@ -1,42 +1,10 @@
-import {Dimensions, StyleSheet} from 'react-native';
-
-const {height} = Dimensions.get('screen');
+import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {
-    width: '100%',
-    height: height * 0.7,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 9,
-    },
-    shadowOpacity: 0.24,
-    shadowRadius: 7,
-
-    elevation: 2,
-
-    borderBottomEndRadius: 25,
-    borderBottomStartRadius: 25,
-  },
-  imageWrapper: {
-    flex: 1,
-    borderBottomEndRadius: 25,
-    borderBottomStartRadius: 25,
-    overflow: 'hidden',
-  },
-  image: {
-    flex: 1,
-  },
   detailsWrapper: {
     margin: 20,
   },
-  originalTitle: {
-    opacity: 0.7,
-    fontSize: 14,
-  },
-  detailsTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  loading: {
+    marginTop: 20,
   },
 });
