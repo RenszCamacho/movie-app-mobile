1a5053cfeb11c14696f456ad2c3818369462095a
Renszo Camacho <varsovia84@gmail.com>
2021-11-28 11:53:15 +0100

    Create useMovieDetails.

---
+++ src/hooks/useMovieDetails.tsx
@@ -0,0 +1,44 @@
+import {useEffect, useState} from 'react';
+import movieDB from '../api/movieDB';
+import {Cast, CreditsResponse} from '../types/creditDetails.interface';
+import {FullDetails} from '../types/fullDetails.interface';
+
+interface MoviesDetails {
+  isLoading: boolean;
+  movieDetails: FullDetails | undefined;
+  cast: Cast[];
+}
+
+export default function useMovieDetails(movieId: number) {
+  const [movie, setMovie] = useState<MoviesDetails>({
+    isLoading: true,
+    movieDetails: undefined,
+    cast: [],
+  });
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      const fullDatailsPromise = await movieDB.get<FullDetails>(`/${movieId}`);
+      const castDetailsPromise = await movieDB.get<CreditsResponse>(
+        `/${movieId}/credits`,
+      );
+
+      const [fullDetailsResponse, castDetailsResponse] = await Promise.all([
+        fullDatailsPromise,
+        castDetailsPromise,
+      ]);
+
+      setMovie({
+        isLoading: false,
+        movieDetails: fullDetailsResponse.data,
+        cast: castDetailsResponse.data.cast,
+      });
+    };
+
+    fetchMovie();
+  }, [movieId]);
+
+  return {
+    ...movie,
+  };
+}
